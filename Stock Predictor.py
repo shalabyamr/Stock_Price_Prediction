c26e0170dc46458385124ddddc4fd04f71d73a01
Amr Shalaby <amrshalaby@live.ca>
2025-11-06 09:11:37 -0500

    create /results/ folder to save the results and plots

--- Stock Predictor.py
+++ Stock Predictor.py
@@ -36,23 +36,23 @@ forecast["ds"] = pd.to_datetime(forecast['ds'])
 forecast["ds"] = forecast["ds"].dt.normalize() #here
 today = pd.to_datetime('today').normalize()
 forecast["yhat"] = forecast["yhat"].round(2)
-forecast[["ds", "yhat"]].to_csv(f"{ticker}_historical_and_forecast.csv", index=False)
+forecast[["ds", "yhat"]].to_csv(f"results/{ticker}_historical_and_forecast.csv", index=False)
 forecast = forecast[forecast['ds'] >= today]
 # -----------------------------------------
 # 4. Show results
 # -----------------------------------------
 print("Future price prediction:")
 print(forecast[["ds", "yhat"]].tail(90))
-forecast[forecast['ds'] >= today][["ds", "yhat"]].to_csv(f"{ticker}_forecast.csv", index=False)
+forecast[forecast['ds'] >= today][["ds", "yhat"]].to_csv(f"results/{ticker}_forecast.csv", index=False)
 
 # Plot full forecast
 forecast = forecast[forecast["ds"] >= today]
 model.plot(forecast, xlabel="Date", ylabel="DSV STOCK PREDICTION", include_legend=True, plot_cap=True)
 plt.title(f"DSV.TO Stock Forecast ({forecast_period} days forward)")
-plt.savefig(f"{ticker}_forecast.png")
+plt.savefig(f"results/{ticker}_forecast.png")
 plt.show()
 # Plot trends/components (seasonality, trend strength, etc.)
 model.plot_components(forecast)
-plt.savefig(f"{ticker}_components.png")
+plt.savefig(f"results/{ticker}_components.png")
 plt.show()
 
